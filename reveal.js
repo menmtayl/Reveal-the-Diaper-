3aead81ffea5ad25067d41c8515d4f618932d182
Mendell Taylor <menmtayl@gmail.com>
2019-03-25 19:23:35 -0400

    first commit

---
+++ reveal.js
@@ -0,0 +1,11 @@
+    //$('.Gender-Reveal').on('click','button',function(){
+    //reveal text
+    $('.Reveal span').hide();
+    $('.Reveal button').click(function(){
+    $('.Reveal span').show();
+    $('.Reveal button').hide();
+
+});
+   // creating the gender reveal button
+    //const $button = $('<button> Gender Reveal</button>');
+    //$('.Gender-Reveal').append($button);
